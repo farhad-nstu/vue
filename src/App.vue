21b3fec59a1bef0b3b15aa09d5252a6d6f86a0b4
farhad-nstu <amfarhad33@gmail.com>
2019-12-03 08:45:11 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <div id="loading" v-if="loading">
+      <img src="/static/img/source.gif">
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return {
+      loading: false
+    }
+  },
+
+  created(){
+    this.$eventBus.$on("loadingStatus", payload=> {
+      this.loading = payload;
+    });
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/pages/Login.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="login">
+    <div class="loginHeader">
+      User Login
+    </div>
+    <div class="containerHeader">
+      <table>
+        <tr>
+          <td>User Name</td>
+          <td>:</td>
+          <td><input type="text" placeholder="User Name" v-model="user.username"></td>
+        </tr>
+
+        <tr>
+          <td>Password</td>
+          <td>:</td>
+          <td><input type="password" name="password" v-model="user.password"></td>
+        </tr>
+
+        <tr>
+          <td></td>
+          <td>:</td>
+          <td><button class="addBtn" @click="loginNow()">Login</button></td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      user: {
+        username: "",
+        password: ""
+      }
+    }
+  },
+
+  methods: {
+    loginNow(){
+      console.log(this.user);
+      this.$eventBus.$emit("loadingStatus", true);
+
+      this.$axios.post("http://rimonbd.com/tutorial/api/login", this.user)
+       .then(res=> {
+        this.$eventBus.$emit("loadingStatus", false);
+        console.log(res);
+      });
+    }
+  }
+} 
+</script>
